483feeb19b6867c41f734565a8236f9cdf5944bd
BobDoleOwndU <spikey5556@hotmail.com>
2017-03-24 20:18:49 -0400

    Increased drop down list widths.

--- MQPMTool2/MainForm.Designer.cs
+++ MQPMTool2/MainForm.Designer.cs
@@ -59,27 +59,29 @@
             // playerOutfitComboBox
             // 
             this.playerOutfitComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.playerOutfitComboBox.DropDownWidth = 175;
             this.playerOutfitComboBox.FormattingEnabled = true;
             this.playerOutfitComboBox.Location = new System.Drawing.Point(12, 127);
             this.playerOutfitComboBox.Name = "playerOutfitComboBox";
-            this.playerOutfitComboBox.Size = new System.Drawing.Size(175, 21);
+            this.playerOutfitComboBox.Size = new System.Drawing.Size(121, 21);
             this.playerOutfitComboBox.TabIndex = 2;
             this.playerOutfitComboBox.SelectedIndexChanged += new System.EventHandler(this.playerOutfitComboBox_SelectedIndexChanged);
             // 
             // characterOutfitComboBox
             // 
             this.characterOutfitComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.characterOutfitComboBox.DropDownWidth = 175;
             this.characterOutfitComboBox.FormattingEnabled = true;
-            this.characterOutfitComboBox.Location = new System.Drawing.Point(197, 127);
+            this.characterOutfitComboBox.Location = new System.Drawing.Point(139, 127);
             this.characterOutfitComboBox.Name = "characterOutfitComboBox";
-            this.characterOutfitComboBox.Size = new System.Drawing.Size(175, 21);
+            this.characterOutfitComboBox.Size = new System.Drawing.Size(121, 21);
             this.characterOutfitComboBox.TabIndex = 3;
             this.characterOutfitComboBox.SelectedIndexChanged += new System.EventHandler(this.characterOutfitComboBox_SelectedIndexChanged);
             // 
             // quietOutfitLabel
             // 
             this.quietOutfitLabel.AutoSize = true;
-            this.quietOutfitLabel.Location = new System.Drawing.Point(194, 111);
+            this.quietOutfitLabel.Location = new System.Drawing.Point(136, 111);
             this.quietOutfitLabel.Name = "quietOutfitLabel";
             this.quietOutfitLabel.Size = new System.Drawing.Size(88, 13);
             this.quietOutfitLabel.TabIndex = 4;
@@ -97,7 +99,7 @@
             // hipWeaponLabel
             // 
             this.hipWeaponLabel.AutoSize = true;
-            this.hipWeaponLabel.Location = new System.Drawing.Point(194, 151);
+            this.hipWeaponLabel.Location = new System.Drawing.Point(136, 151);
             this.hipWeaponLabel.Name = "hipWeaponLabel";
             this.hipWeaponLabel.Size = new System.Drawing.Size(72, 13);
             this.hipWeaponLabel.TabIndex = 6;
@@ -106,26 +108,28 @@
             // headComboBox
             // 
             this.headComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.headComboBox.DropDownWidth = 175;
             this.headComboBox.FormattingEnabled = true;
             this.headComboBox.Location = new System.Drawing.Point(12, 167);
             this.headComboBox.Name = "headComboBox";
-            this.headComboBox.Size = new System.Drawing.Size(175, 21);
+            this.headComboBox.Size = new System.Drawing.Size(121, 21);
             this.headComboBox.TabIndex = 7;
             this.headComboBox.SelectedIndexChanged += new System.EventHandler(this.headComboBox_SelectedIndexChanged);
             // 
             // hipComboBox
             // 
             this.hipComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.hipComboBox.DropDownWidth = 175;
             this.hipComboBox.FormattingEnabled = true;
-            this.hipComboBox.Location = new System.Drawing.Point(197, 167);
+            this.hipComboBox.Location = new System.Drawing.Point(139, 167);
             this.hipComboBox.Name = "hipComboBox";
-            this.hipComboBox.Size = new System.Drawing.Size(175, 21);
+            this.hipComboBox.Size = new System.Drawing.Size(121, 21);
             this.hipComboBox.TabIndex = 8;
             this.hipComboBox.SelectedIndexChanged += new System.EventHandler(this.hipComboBox_SelectedIndexChanged);
             // 
             // processButton
             // 
-            this.processButton.Location = new System.Drawing.Point(13, 194);
+            this.processButton.Location = new System.Drawing.Point(12, 194);
             this.processButton.Name = "processButton";
             this.processButton.Size = new System.Drawing.Size(75, 23);
             this.processButton.TabIndex = 9;
@@ -137,12 +141,12 @@
             // 
             this.outputTextBox.Location = new System.Drawing.Point(12, 29);
             this.outputTextBox.Name = "outputTextBox";
-            this.outputTextBox.Size = new System.Drawing.Size(329, 20);
+            this.outputTextBox.Size = new System.Drawing.Size(217, 20);
             this.outputTextBox.TabIndex = 11;
             // 
             // outputButton
             // 
-            this.outputButton.Location = new System.Drawing.Point(347, 24);
+            this.outputButton.Location = new System.Drawing.Point(235, 26);
             this.outputButton.Name = "outputButton";
             this.outputButton.Size = new System.Drawing.Size(25, 25);
             this.outputButton.TabIndex = 12;
@@ -163,9 +167,9 @@
             // 
             this.characterComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.characterComboBox.FormattingEnabled = true;
-            this.characterComboBox.Location = new System.Drawing.Point(103, 19);
+            this.characterComboBox.Location = new System.Drawing.Point(64, 19);
             this.characterComboBox.Name = "characterComboBox";
-            this.characterComboBox.Size = new System.Drawing.Size(175, 21);
+            this.characterComboBox.Size = new System.Drawing.Size(121, 21);
             this.characterComboBox.TabIndex = 15;
             this.characterComboBox.SelectedIndexChanged += new System.EventHandler(this.characterComboBox_SelectedIndexChanged);
             // 
@@ -174,7 +178,7 @@
             this.CharacterGroupBox.Controls.Add(this.characterComboBox);
             this.CharacterGroupBox.Location = new System.Drawing.Point(12, 56);
             this.CharacterGroupBox.Name = "CharacterGroupBox";
-            this.CharacterGroupBox.Size = new System.Drawing.Size(360, 52);
+            this.CharacterGroupBox.Size = new System.Drawing.Size(248, 52);
             this.CharacterGroupBox.TabIndex = 16;
             this.CharacterGroupBox.TabStop = false;
             this.CharacterGroupBox.Text = "Player Character";
@@ -182,7 +186,7 @@
             // authorLabel
             // 
             this.authorLabel.AutoSize = true;
-            this.authorLabel.Location = new System.Drawing.Point(234, 239);
+            this.authorLabel.Location = new System.Drawing.Point(124, 299);
             this.authorLabel.Name = "authorLabel";
             this.authorLabel.Size = new System.Drawing.Size(138, 13);
             this.authorLabel.TabIndex = 17;
@@ -192,7 +196,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.ClientSize = new System.Drawing.Size(274, 321);
             this.Controls.Add(this.authorLabel);
             this.Controls.Add(this.CharacterGroupBox);
             this.Controls.Add(this.outputLabel);
